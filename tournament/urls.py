cc81b6af996b3bd3c3789d51274d2554f5d64245
Manish Gautam <gautammanish700@gmail.com>
2023-06-09 14:35:46 +0545

    ccc

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.urls import include, path
-
+# urls for tournament
 urlpatterns = [
     path('event/', include('tournament.event_urls')),
     path('tournament/', include('tournament.tournament_urls')),
